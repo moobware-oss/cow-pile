4472d606f5d47ea731af7b9be4011a378863e78c
Moobware Dev <dev@moobware.com>
2019-10-26 12:49:40 -0500

    [Feature] Add head collider, turn off gravity conditionally

---
+++ Assets/Programming/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public bool GravityEnabled;
+    private Vector3 _originalGravity;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _originalGravity = Physics.gravity;
+        SetGravity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetGravity();
+    }
+
+    void SetGravity()
+    {
+        if (GravityEnabled && Physics.gravity != _originalGravity)
+        {
+            Physics.gravity = _originalGravity;
+        }
+        else
+        {
+            Physics.gravity = Vector3.zero;
+        }
+    }
+}
